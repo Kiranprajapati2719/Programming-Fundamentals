6f5e0a5a62a39aab9d88977c764c171d470867fd
Kiranprajapati2719 <kiranprajapati2719@gmail.com>
2016-11-21 11:34:38 +0545

    Add files via upload

---
+++ K4L1.c
@@ -0,0 +1,25 @@
+/*
+ Student Name: Kiran Prajapati
+ Subject: Programming Fundamental
+ Roll:
+ Program: To print area of rectangle
+ Lab Sheet:
+ Date: 21th Nov. 2016
+ */
+ 
+ #include <stdio.h>
+ #include <math.h>
+  
+ int main () {
+	 int l,b;
+	 float area;
+	 printf("Enter the length of rectangle");
+	 scanf("%d",&l);
+	 printf("Enter the breath of rectangle");
+	 scanf("%d",&b);
+	 area=l*b;
+	 printf("Area of the rectangle is %f" , area);
+	 return (0);
+ }
+	 
+	  
