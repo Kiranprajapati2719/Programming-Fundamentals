eb019eb2f1f433dbda685e4521933613c2f41075
Kiranprajapati2719 <kiranprajapati2719@gmail.com>
2016-11-21 13:04:47 +0545

    Add files via upload

---
+++ Rectangle(area).c
@@ -0,0 +1,24 @@
+/*
+ Student Name: Kiran Prajapati
+ Subject: Programming Fundamental
+ Roll:
+ Program: To print area and perimeter of rectangle
+ Lab Sheet:
+ Date: 21th Nov. 2016
+ */
+ 
+#include<stdio.h>
+
+int main(){
+	int l,b;
+	float area;
+	printf("Enter the length:\n");
+	scanf("%d",&l);
+	printf("Enter the breath:\n");
+	scanf("%d",&b);
+	area=l*b;
+	printf("Area of Rectangle is :%f",area);
+	return(0);
+	
+	
+	}
