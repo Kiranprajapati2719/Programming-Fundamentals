0769e4fdd8e939de18375a849859471ee05d08bb
Kiranprajapati2719 <kiranprajapati2719@gmail.com>
2016-11-22 23:05:14 -0800

    Add files via upload

---
+++ ASCII.c
@@ -0,0 +1,22 @@
+/*
+Student name: Kiran Prajapati
+Subject: Programming Fundamental
+Roll:
+Program: WAP to find ASCII value of any character assigned from user
+Lab sheet no:
+Date: 2016 Nov 23th
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    char character;
+    int asciivalue;
+    printf("Enter a character:");
+    scanf("%c",&character);
+    asciivalue=character;
+    printf("The character is :%d",asciivalue);
+    getch();
+    return(0);
+}
