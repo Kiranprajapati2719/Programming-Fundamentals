ff81ed23fa7aac817f6faf2c3e6719862b808b92
Kiranprajapati2719 <kiranprajapati2719@gmail.com>
2016-11-23 11:03:24 +0545

    Add files via upload

---
+++ celcius2fahrenheit.c
@@ -0,0 +1,19 @@
+/*
+Student name: Kiran Prajapati
+Subject: Programming Fundamental
+Roll:
+Program: WAP to program to print sum of three digits number
+Lab sheet no:
+Date: 2016 Nov 23th
+*/
+
+#include<stdio.h>
+
+int main(){
+    float c,fahren;
+    printf("Enter the temperature in Celcius:\n");
+    scanf("%f",&c);
+    fahren=(c*1.8)+32;
+    printf("The temperature in Fahrenheit is :%f",fahren);
+    return 0;
+}
