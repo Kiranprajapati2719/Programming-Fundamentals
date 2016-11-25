bdc30eb34b4ba96f5092c8479b839276f970885f
Kiranprajapati2719 <kiranprajapati2719@gmail.com>
2016-11-25 10:24:29 +0545

    Update arithmetic_operation.c

--- arithmetic_operation.c
+++ arithmetic_operation.c
@@ -1,28 +1,34 @@
 /*
-
+Student name: Kiran Prajapati
+Subject: Programming Fundamental
+Roll:
+Program: WAP to convert temperature from degree celcius to fahreinheit
+Lab sheet no:
+04
+Date: 2016 Nov 23th
 */
 
 #include<stdio.h>
+
 int main(){
 
         int sum,sub,a,b,mul,div,mod;
-            printf("Enter the first number:\n");
-                scanf("%d",&a);
-            printf("Enter the second number:\n");
+        printf("Enter the first number:\n");
+        scanf("%d",&a);
+        printf("Enter the second number:\n");
         scanf("%d",&b);
 
-    sum=a+b;
-    sub=a-b;
-    mul=a*b;
-    div=a/b;
-    mod=a%b;
+        sum=a+b;
+        sub=a-b;
+        mul=a*b;
+        div=a/b;
+        mod=a%b;
 
-    printf("The sum is: %d \n", sum);
+        printf("The sum is: %d \n", sum);
         printf("The difference is: %d \n", sub);
-            printf("The product is: %d \n", mul);
-                printf("The division is: %d \n", div);
-                    printf("The modulus is: %d \n", mod);
-                        getch();
-                            return(0);
+        printf("The product is: %d \n", mul);
+        printf("The division is: %d \n", div);
+        printf("The modulus is: %d \n", mod);
+        return(0);
 
 }
