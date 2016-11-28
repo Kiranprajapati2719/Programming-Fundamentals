d1adf125fa98d0a75c97c9809b653263063cb6fd
Kiran Prajapati <kiranprajapati2719@gmail.com>
2016-11-28 12:33:48 +0545

    Add files via upload

---
+++ square_root.c
@@ -0,0 +1,24 @@
+/*
+Student name: Kiran Prajapati
+Subject: Programming Fundamental
+Roll:
+Program: WAP to program to enter any number and calculate its square root
+Lab sheet no:06
+Date: 2016 Nov 23th
+*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+
+int main() {
+    float a, sq;
+    printf("Enter the number:\n");
+    scanf("%f",&a);
+    sq=pow(a,0.5);
+    printf("The square root of given number is: %f \n",sq);
+    getch();
+    return(0);
+
+}
