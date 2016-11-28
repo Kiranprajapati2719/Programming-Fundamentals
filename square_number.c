2308ad7dc80198598db979f0b0d48df39eb74f47
Kiran Prajapati <kiranprajapati2719@gmail.com>
2016-11-28 12:25:49 +0545

    Add files via upload

---
+++ square_number.c
@@ -0,0 +1,21 @@
+/*
+Student name: Kiran Prajapati
+Subject: Programming Fundamental
+Roll:
+Program: WAP to program to enter any number and calculate its square
+Lab sheet no:
+Date: 2016 Nov 23th
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+    float a,sq;
+    printf("Enter a number:\n");
+    scanf("%f",&a);
+    sq=a*a;
+    printf("The square of given number is %f \n",sq);
+    getch();
+    return(0);
+}
