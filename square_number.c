568eb2a9ab1a902fbc089483be74b9db7506bcfe
Kiran Prajapati <kiranprajapati2719@gmail.com>
2016-11-28 12:47:11 +0545

    Update square_number.c

--- square_number.c
+++ square_number.c
@@ -9,12 +9,13 @@ Date: 2016 Nov 23th
 
 #include<stdio.h>
 #include<conio.h>
+#include<math.h>
 
 int main() {
     float a,sq;
     printf("Enter a number:\n");
     scanf("%f",&a);
-    sq=a*a;
+    sq=pow(a,2);
     printf("The square of given number is %f \n",sq);
     getch();
     return(0);
