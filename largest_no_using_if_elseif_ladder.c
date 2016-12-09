7c59019e68a628ccb35bd3884fa764a97b796a69
Kiran Prajapati <kiranprajapati2719@gmail.com>
2016-12-09 10:16:16 +0545

    Add files via upload

---
+++ largest_no_using_if_elseif_ladder.c
@@ -0,0 +1,42 @@
+/*
+Student Name: Kiran Prajapati
+Subject:Programming Fundamental
+Program: WAP to 
+Roll No.: 15
+Lab sheet No.: 12
+Date:2016 Dec 09
+*/
+
+#include<stdio.h>
+
+int main () {
+	int a,b,c;
+	printf("Enter the first number >> ");
+	scanf("%d",&a);
+	printf("Enter the second number >> ");
+	scanf("%d",&b);
+	printf("Enter the third number >> ");
+	scanf("%d",&c);
+	
+	if(a>=b&&a>=c)
+		{
+			printf("The number %d is the largest",a);
+		}
+	else if(b>=a&&b>=c)
+		{
+			printf("The number %d is the largest",b);
+		}
+	else(c>=b&&c>=a)
+		{
+			printf("The number %d is the largest",c);
+		}	
+
+	return 0;
+	}
+
+
+
+
+
+
+
