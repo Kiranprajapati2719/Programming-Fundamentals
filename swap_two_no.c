a46d8e24a237742eda96bac2e5a159658737aca5
Kiran Prajapati <kiranprajapati2719@gmail.com>
2016-12-14 09:58:53 +0545

    Add files via upload

---
+++ swap_two_no.c
@@ -0,0 +1,29 @@
+/*
+Student nsme: Kiran Prajapati
+Subject: Programming Fundamentals
+Roll: 15
+Program: WAP to swap two values
+Date: 14 Dec 2016
+*/
+
+#include<stdio.h>
+
+int main () {
+    int a=0,b=0,temp=0;
+
+    printf("Enter the first value >> ");
+    scanf("%d",&a);
+
+    printf("Enter the second value >>");
+    scanf("%d",&b);
+
+    printf("\nBefore Swap %d and %d \n",a,b);
+
+    temp=a;
+    a=b;
+    b=temp;
+
+    printf("\nAfter Swap %d and %d \n",a,b);
+
+    return(0);
+}
