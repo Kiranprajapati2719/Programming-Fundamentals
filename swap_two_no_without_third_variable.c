760c5d976dfe18cfcb93d96c7c68a9d972ebfbd0
Kiran Prajapati <kiranprajapati2719@gmail.com>
2016-12-14 10:20:06 +0545

    Add files via upload

---
+++ swap_two_no_without_third_variable.c
@@ -0,0 +1,33 @@
+
+/*
+Student nsme: Kiran Prajapati
+Subject: Programming Fundamentals
+Roll: 15
+Program: WAP to swap two values without using
+Date: 14 Dec 2016
+*/
+
+#include<stdio.h>
+
+int main () {
+    int a=0,b=0;
+
+    printf("Enter the first value >> ");
+    scanf("%d",&a);
+
+    printf("Enter the second value >> ");
+    scanf("%d",&b);
+
+    printf("\nBefore Swap %d and %d \n",a,b);
+
+    a=a-b;
+    b=a+b;
+    a=b-a;
+
+
+
+
+    printf("\nAfter Swap %d and %d \n",a,b);
+
+    return(0);
+}
